aa0bb900c985604fcc0cc7aff42348c7dabc0277
Herschel Pawar <pawarherschel@gmail.com>
2023-05-14 23:32:08 +0530

    slowly switching to daisyui

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,26 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
+  daisyui: {
+    themes: [
+      {
+        KatPilled: {
+          primary: '#fbcfe8',
+          secondary: '#D926AA',
+          accent: '#1FB2A5',
+          neutral: '#191D24',
+          'base-100': '#2A303C',
+          info: '#3ABFF8',
+          success: '#36D399',
+          warning: '#FBBD23',
+          error: '#F87272',
+        },
+      },
+    ],
+    darkTheme: "KatPilled",
+  },
   content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
   theme: {
     extend: {},
   },
-  plugins: [],
-  darkMode: ['class', '.darkmode'],
-}
+  plugins: [require("@tailwindcss/typography"), require("daisyui")],
+};
